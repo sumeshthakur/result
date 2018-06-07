976fe72789da334d823bcb52e3fa4d5f458923c0
Sumesh Thakur <34761608+sumeshthakur@users.noreply.github.com>
2018-06-07 16:17:19 +0530

    Update and rename buefy.js to bira.js

---
+++ bira.js
@@ -0,0 +1,53 @@
+Vue.use(Buefy.default)
+
+
+
+var v = new Vue({
+  el:'#structure',
+  data:{
+    DataResults:[],
+    NumberWebpage:'',
+    },
+
+  methods:{
+    alert() {
+     this.$dialog.alert('Everything looks fine!')
+    },
+    alertCustom() {
+    this.$dialog.alert({
+     title: 'Title Alert',
+     message: 'I have a title, a custom button and <b>HTML</b>!',
+     confirmText: 'Cool!'
+     })
+    },
+    alertCustomError() {
+     this.$dialog.alert({
+     title: 'Error',
+     message: 'Something\'s not good but I have a custom <b>icon</b> and <b>type</b>',
+     type: 'is-danger',
+     hasIcon: true,
+     icon: 'times-circle',
+     iconPack: 'fa'
+     })
+    }
+
+  },
+
+  computed:{
+
+  },
+  created: function () {
+  fetch("/db.json")
+    .then(r => r.json())
+    .then(json =>{
+      this.DataResults=json;
+    var Urls=[];
+      this.NumberWebpage=json.length;
+
+      console.log(Urls);
+
+    });
+   }
+
+
+});
