d75fa2d826512aa8d8751daf919fb9a70261e5b4
Sumesh Thakur <34761608+sumeshthakur@users.noreply.github.com>
2018-06-07 16:00:33 +0530

    Update buefy.js

--- buefy.js
+++ buefy.js
@@ -1,7 +1,30 @@
 
 Vue.use(Buefy.default)
 
-
+const example = {
+        methods: {
+            alert() {
+this.$dialog.alert('Everything looks fine!')
+            },
+            alertCustom() {
+this.$dialog.alert({
+    title: 'Title Alert',
+    message: 'I have a title, a custom button and <b>HTML</b>!',
+    confirmText: 'Cool!'
+})
+            },
+            alertCustomError() {
+this.$dialog.alert({
+    title: 'Error',
+    message: 'Something\'s not good but I have a custom <b>icon</b> and <b>type</b>',
+    type: 'is-danger',
+    hasIcon: true,
+    icon: 'times-circle',
+    iconPack: 'fa'
+})
+            }
+        }
+    }
 
 
 var v = new Vue({
